e26f9774da8a9307883ad644cb1e719f78c6b54b
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-07-06 15:14:53 +0300

    display recipe function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Get references to HTML elements
 const searchBtn = document.getElementById('search-btn');
 const searchInput = document.querySelector('input[type="text"]');
 const recipeContainer = document.getElementById('recipe-container');
+const perPage = 3; // Number of recipes to display per page
+let currentPage = 1; // Current page of results
 
 // Add event listener to search button
 searchBtn.addEventListener('click', searchRecipes);
@@ -20,7 +21,10 @@ function searchRecipes() {
     .then(response => response.json())
     .then(data => {
       // Process the API response and display the results on the page
-      displayRecipes(data.results);
+      const startIndex = (currentPage - 1) * perPage;
+      const endIndex = startIndex + perPage;
+      const recipes = data.results.slice(startIndex, endIndex);
+      displayRecipes(recipes);
     })
     .catch(error => {
       console.log('Error:', error);
@@ -32,15 +36,33 @@ function displayRecipes(recipes) {
   // Clear previous results
   recipeContainer.innerHTML = '';
 
-  // Loop through the recipes and create HTML elements for each recipe
-  recipes.forEach(recipe => {
+  // Loop through the recipes for the current page
+  for (let i = 0; i < recipes.length; i++) {
+    // Create a recipe card element
     const recipeCard = document.createElement('div');
     recipeCard.classList.add('recipe-card');
     recipeCard.innerHTML = `
-      <img src="${recipe.image}" alt="${recipe.title}" />
-      <h3>${recipe.title}</h3>
+      <img src="${recipes[i].image}" alt="${recipes[i].title}" />
+      <h3>${recipes[i].title}</h3>
     `;
     recipeContainer.appendChild(recipeCard);
-  });
+  }
 }
 
+// Function to handle pagination navigation
+function goToPage(page) {
+  currentPage = page;
+  searchRecipes();
+}
+
+// Example usage: Go to next page
+const nextPageButton = document.getElementById('next-page');
+nextPageButton.addEventListener('click', () => {
+  goToPage(currentPage + 1);
+});
+
+// Example usage: Go to previous page
+const prevPageButton = document.getElementById('prev-page');
+prevPageButton.addEventListener('click', () => {
+  goToPage(currentPage - 1);
+});
