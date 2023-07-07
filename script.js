b7926dbc099e8f94c82b01e4b6c869f7c74c46ac
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-07-07 12:59:07 +0300

    deployment

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// client.js (Client-side code)
-
-// Select the elements using DOM methods
 const searchBtn = document.getElementById('search-btn');
 const searchInput = document.querySelector('input[type="text"]');
 const recipeContainer = document.getElementById('recipe-container');
+const perPage = 3; // Number of recipes to display per page
+let currentPage = 1; // Current page of results
 
 // Add event listener to search button
 searchBtn.addEventListener('click', searchRecipes);
@@ -17,7 +16,10 @@ function searchRecipes() {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      displayRecipes(data.results);
+      const startIndex = (currentPage - 1) * perPage;
+      const endIndex = startIndex + perPage;
+      const recipes = data.results.slice(startIndex, endIndex);
+      displayRecipes(recipes);
     })
     .catch(error => {
       console.log('Error:', error);
@@ -28,20 +30,21 @@ function searchRecipes() {
 function displayRecipes(recipes) {
   recipeContainer.innerHTML = '';
 
-  recipes.forEach(recipe => {
+  for (let i = 0; i < recipes.length; i++) {
     const recipeCard = document.createElement('div');
     recipeCard.classList.add('recipe-card');
     recipeCard.innerHTML = `
-      <img src="${recipe.image}" alt="${recipe.title}" />
-      <h3>${recipe.title}</h3>
+      <img src="${recipes[i].image}" alt="${recipes[i].title}" />
+      <h3>${recipes[i].title}</h3>
+      
     `;
 
     recipeCard.addEventListener('click', () => {
-      showRecipeDetails(recipe.id);
+      showRecipeDetails(recipes[i].id);
     });
 
     recipeContainer.appendChild(recipeCard);
-  });
+  }
 }
 
 // Function to show recipe details
@@ -52,10 +55,127 @@ function showRecipeDetails(recipeId) {
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      // Display recipe details
-      console.log(data);
+      const recipeImage = document.createElement('img');
+      recipeImage.src = data.image;
+      recipeImage.alt = data.title;
+
+      const recipeTitle = document.createElement('h3');
+      recipeTitle.textContent = data.title;
+
+      const recipeDetails = document.createElement('div');
+      recipeDetails.innerHTML = `
+        <h4>Ready in ${data.readyInMinutes} minutes</h4>
+        <p>Servings: ${data.servings}</p>
+        <h4>Ingredients:</h4>
+        <ul>
+          ${data.extendedIngredients.map(ingredient => `<li>${ingredient.original}</li>`).join('')}
+        </ul>
+        <h4>Method:</h4>
+        <ol>
+          ${data.analyzedInstructions[0].steps.map(step => `<li>${step.step}</li>`).join('')}
+        </ol>
+        <button class="download-btn">Download</button>
+      `;
+
+      const downloadButton = recipeDetails.querySelector('.download-btn');
+      downloadButton.addEventListener('click', () => {
+        downloadRecipe(data);
+      });
+
+      recipeContainer.innerHTML = '';
+      recipeContainer.appendChild(recipeImage);
+      recipeContainer.appendChild(recipeTitle);
+      recipeContainer.appendChild(recipeDetails);
     })
     .catch(error => {
       console.log('Error:', error);
+});
+}
+
+// Function to handle pagination navigation
+function goToPage(page) {
+  currentPage = page;
+  searchRecipes();
+}
+
+// Function to save a recipe
+function saveRecipe(recipe) {
+  const savedRecipes = getSavedRecipes();
+  savedRecipes.push(recipe);
+  saveRecipesLocally(savedRecipes);
+  console.log('Recipe saved:', recipe);
+}
+
+// Function to get saved recipes from local storage
+function getSavedRecipes() {
+  const savedRecipesData = localStorage.getItem('savedRecipes');
+  if (savedRecipesData) {
+    return JSON.parse(savedRecipesData);
+  }
+  return [];
+}
+
+// Function to save recipes to local storage
+function saveRecipesLocally(savedRecipes) {
+  localStorage.setItem('savedRecipes', JSON.stringify(savedRecipes));
+}
+
+// Function to download a recipe as a JSON file
+function downloadRecipe(recipe) {
+  const recipeData = JSON.stringify(recipe);
+  const blob = new Blob([recipeData], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = `${recipe.title}.json`;
+  link.click();
+  URL.revokeObjectURL(url);
+}
+
+// Call the function to load saved recipes on page load
+loadSavedRecipes();
+
+const recipeForm = document.getElementById('recipe-form');
+
+// Add event listener to recipe form
+recipeForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent form submission
+
+  // Get user input from the form inputs
+  const recipeInput = document.getElementById('recipe-input');
+  const instructionsInput = document.getElementById('instructions-input');
+  const title = titleInput.value;
+  const instructions = instructionsInput.value;
+
+  // Call the createRecipe function
+  createRecipe(title, instructions);
+});
+
+// Function to create a recipe
+function createRecipe(title, instructions) {
+  const apiKey = 'd40b55e0b55f4099b859b04b2533d17a'; 
+
+  const apiUrl = 'https://api.spoonacular.com/recipes/visualizeRecipe';
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('instructions', instructions);
+  formData.append('apiKey', apiKey);
+
+  // Send a POST request to the API
+  fetch(apiUrl, {
+    method: 'POST',
+    body: formData,
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Recipe created:', data);
+      // Handle the response and perform any necessary actions
+    })
+    .catch(error => {
+      console.log('Error:', error);
+      // Handle the error
     });
 }
+
+
+    
--- server.js
+++ server.js
@@ -1,7 +1,41 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-const document = dom.window.document;
+const server = http.createServer((req, res) => {
+  // Determine the file path based on the requested URL
+  let filePath = path.join(__/'Development/Code/ann/The-Munchies-Recipe-App-Phase-1-project-', req.url === '/' ? 'index.html' : req.url);
 
-console.log(document.querySelector("p").textContent); // Output: "Hello world"
+  // Check if the requested file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // If the file doesn't exist, return a 404 error
+      res.statusCode = 404;
+      res.end('404 Not Found');
+    } else {
+      // Read the file and return its contents
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('500 Internal Server Error');
+        } else {
+          // Set the appropriate Content-Type header based on the file extension
+          let contentType = 'text/html';
+          if (filePath.endsWith('.css')) {
+            contentType = 'text/css';
+          } else if (filePath.endsWith('.js')) {
+            contentType = 'text/javascript';
+          }
+
+          res.setHeader('Content-Type', contentType);
+          res.end(data);
+        }
+      });
+    }
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
